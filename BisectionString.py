ebc160a7eca4802a3b32d4b3bc02f528a12e6b4e
root <root@justdial.(none)>
2017-01-02 19:48:49 +0530

    Small Algorithm

--- BisectionString.py
+++ BisectionString.py
@@ -6,4 +6,19 @@ def isIn(char, aStr):
     returns: True if char is in aStr; False otherwise
     '''
     # Your code here
-    
+    if len(aStr) == 0 or len(char) == 0:
+        return False
+    elif len(aStr) == 1:
+        return char == aStr
+    else:
+        mid = int(len(aStr) / 2)
+        if aStr[mid] == char:
+            return True
+        else:
+            if char > aStr[mid]:
+                return isIn(char, aStr[mid:])
+            else:
+                return isIn(char, aStr[0:mid])
+
+
+print(isIn('p','abcdefghijklmnop'))
